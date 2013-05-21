9d69d6172793b89db9b9885ffb8f3fa4759ad624
Luka Čehovin <luka.cehovin@gmail.com>
2013-05-21 18:44:12 +0200

    Simple testing script.

---
+++ evaluation/track_test.m
@@ -0,0 +1,16 @@
+
+% This file serves only as a basic test for some functionality and has to be removed soon.
+
+util_dir = fullfile(pwd, 'utilities');
+rmpath(util_dir); addpath(util_dir);
+
+test_sequence = '<TODO: set path to sequence directory>';
+
+track_dummy = fullfile(pwd, '..', 'examples', 'c', 'track_dummy');
+
+tracker = track_create_tracker(track_dummy);
+sequence = track_create_sequence(test_sequence);
+
+[trajectory, time] = track_trial(tracker, sequence, 1);
+
+time
