c8b9c61b49c98f3f4cb4f4996bc84ff609eb3f90
Georg Nebehay <gnebehay@gmail.com>
2013-05-22 15:25:08 +0200

    Moved call to matlab to track_test.m

--- evaluation/utilities/track_create_tracker.m
+++ evaluation/utilities/track_create_tracker.m
@@ -1,9 +1,3 @@
 function [tracker] = track_create_tracker(command)
 
 tracker = struct('command', command);
-
-%For MATLAB/Octave:
-%tracker = struct('command', ['/path/to/matlab/bin/matlab -nodesktop -nosplash -r ' command]);
-
-
-
