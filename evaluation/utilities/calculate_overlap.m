f7a31ee493fcdb252c15e597635367ab6034903e
Luka Čehovin <luka.cehovin@gmail.com>
2013-05-22 23:56:45 +0200

    Adding overlap calculation function

---
+++ evaluation/utilities/calculate_overlap.m
@@ -0,0 +1,23 @@
+function [overlap] = calculate_overlap(T1, T2)
+% CALCULATE_OVERLAP  Calculates per-frame overlap for two bounding-box trajectories.
+%   OVERLAP = CALCULATE_OVERLAP(T1, T2) calculates overlap between trajectories
+%       T1 and T2, where T1 and T2 are matrices of size N1 x 4 and N2 x 4, where
+%       the corresponding columns for each matrix describe the upper left and top
+%       coordinate as well as width and height of the bounding box. The resulting
+%       vector OVERLAP is of size min(N1, N2) x 1.
+
+len = min(size(T1, 1), size(T2, 1));
+T1 = T1(1:len, :);
+T2 = T2(1:len, :);
+
+intersection = ... 
+   (min([T1(:, 1) + T1(:, 3); T2(:, 1) + T2(:, 3)]) - max([T1(:, 1); T2(:, 1)])) ...
+   .* (min([T1(:, 2) + T1(:, 4); T2(:, 2) + T2(:, 4)]) - max([T1(:, 2); T2(:, 2)]));
+
+intersection(intersection < 0) = 0;
+
+union = (T1(:, 3) .* T1(:, 4)) + (T2(:, 3) .* T2(:, 4)) - intersection;
+
+overlap = intersection ./ union;
+
+overlap(any(isnan(T1)) | any(isnan(T2))) = NaN;
