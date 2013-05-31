bd3b1b90abad790c34c920cd78c01c2bcc82aef7
Luka Čehovin <luka.cehovin@gmail.com>
2013-05-31 13:04:17 +0200

    Changing sequence loading.

--- evaluation/sequence/create_sequence.m
+++ evaluation/sequence/create_sequence.m
@@ -4,13 +4,21 @@ groundtruth_file = fullfile(directory, 'groundtruth.txt');
 
 groundtruth = double(csvread(groundtruth_file));
 
+mask = '%08d.jpg';
+
 sequence = struct('name', name, 'directory', directory, ...
-         'mask', '%08d.jpg', 'groundtruth', groundtruth, 'length', 0);
+        'mask', '%08d.jpg', 'groundtruth', groundtruth, 'length', 0, ...
+        'images', cell(0));
 
 while true
-    if isempty(get_image(sequence, sequence.length + 1))
+    image_name = fsprintf(mask, sequence.length + 1);
+
+    if ~exist(fullfile(sequence.directory, image_name, 'file'))
         break;
     end;
+
+    sequence.images{end+1} = image_name;
+
     sequence.length = sequence.length + 1;
 end;
 
--- evaluation/sequence/get_image.m
+++ evaluation/sequence/get_image.m
@@ -1,9 +1,9 @@
 function [image_path] = get_image(sequence, index)
 
-image_path = fullfile(sequence.directory, sprintf(sequence.mask, index));
-
-if ~exist(image_path, 'file')
+if (sequence.length < index || index < 1)
     image_path = [];
+else
+    image_path = fullfile(sequence.directory, sequence.images{index});
 end;
-
+ 
 
