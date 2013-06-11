74d7ed83e742bff6ba77d85f81673084b5777e82
Luka Čehovin <luka.cehovin@gmail.com>
2013-06-11 15:52:09 +0200

    Saving FPS data for every sequence.

--- evaluation/experiment/repeat_trial.m
+++ evaluation/experiment/repeat_trial.m
@@ -6,6 +6,14 @@ mkpath(directory);
 
 total_time = 0;
 
+time_file = fullfile(directory, sprintf('%s_time.txt', sequence.name));
+
+if track_properties.cache && exist(time_file, 'file')
+    times = csvread(time_file);
+else
+    times = zeros(1, repetitions);
+end;
+
 for i = 1:repetitions
 
     result_file = fullfile(directory, sprintf('%s_%03d.txt', sequence.name, i));
@@ -24,9 +32,13 @@ for i = 1:repetitions
 
     total_time = total_time + t;
     
+    times(i) = 1 / t;
+    
     if ~isempty(trajectory)
         csvwrite(result_file, trajectory);
     end;
 end;
+
+csvwrite(time_file, times);
 
 time = total_time / repetitions;
