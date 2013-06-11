fa08976d760a72212d9f81f4c6d7f18964fed65a
Luka Čehovin <luka.cehovin@gmail.com>
2013-06-11 15:51:27 +0200

    Earlier versions of Matlab do not use second argument for 'system' command, fixing library problems for some trackers.

--- evaluation/experiment/run_tracker.m
+++ evaluation/experiment/run_tracker.m
@@ -1,9 +1,18 @@
 function [trajectory, time] = run_tracker(tracker, sequence, start)
+% RUN_TRACKER  Generates input data for the tracker, runs the tracker and
+%              validates results.
+%
+%   [TRAJECTORY, TIME] = RUN_TRACKER(TRACKER, SEQUENCE, START)
+%              Runs the tracker on a sequence that with a specified offset.
+%
+%   See also RUN_TRIAL, SYSTEM.
 
 % create temporary directory and generate input data
 working_directory = prepare_trial_data(sequence, start);
 
 output_file = fullfile(working_directory, 'output.txt');
+
+library_path = '';
 
 % run the tracker
 old_directory = pwd;
@@ -18,9 +27,22 @@ try
         [status, output] = system(tracker.command, 1);
         time = toc;
     else
-        tic;
-        [status, output] = system(tracker.command, '');
-        time = toc;
+
+		% Save library paths
+		library_path = getenv('LD_LIBRARY_PATH');
+
+		% Make Matlab use system libraries
+		setenv('LD_LIBRARY_PATH', getenv('PATH'));
+
+		if verLessThan('matlab', '7.14.0')
+		    tic;
+		    [status, output] = system(tracker.command);
+		    time = toc;
+		else
+		    tic;
+		    [status, output] = system(tracker.command, '');
+		    time = toc;
+		end;
     end;
         
     if status ~= 0 
@@ -28,6 +50,12 @@ try
     end;
 
 catch e
+
+	% Reassign old library paths if necessary
+	if ~isempty(library_path)
+		setenv('LD_LIBRARY_PATH', library_path);
+	end;
+
     print_debug('ERROR: Exception thrown "%s".', e.message);
 end;
 
@@ -37,6 +65,8 @@ cd(old_directory);
 trajectory = load_trajectory(output_file);
 
 n_frames = size(trajectory, 1);
+
+time = time / (sequence.length-start);
 
 if (n_frames ~= (sequence.length-start) + 1)
     print_debug('WARNING: Tracker did not produce a trajectory file.');
