c9d8cbcf77a5c7d2db3ea2f4db19f6f378159bbc
Luka Čehovin <luka.cehovin@gmail.com>
2013-06-29 21:23:16 +0200

    More information in the manifest.

--- evaluation/tracker/write_manifest.m
+++ evaluation/tracker/write_manifest.m
@@ -4,11 +4,16 @@ mkpath(tracker.directory);
 
 manifest = fullfile(tracker.directory, 'manifest.txt');
 
+[platform_str,platform_maxsize,platform_endian] = computer();
+
 fid = fopen(manifest, 'w');
 
 fprintf(fid, 'toolkit.version=%d\n', toolkit_information());
 fprintf(fid, 'tracker.identifier=%s\n', tracker.identifier);
 fprintf(fid, 'timestamp=%s\n', datestr(now, 31));
+fprintf(fid, 'platfrom=%s\n', platform_str);
+fprintf(fid, 'platfrom.maxsize=%f\n', platform_maxsize);
+fprintf(fid, 'platfrom.endian=%s\n', platform_endian);
 
 
 fclose(fid);
