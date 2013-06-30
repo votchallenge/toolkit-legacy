bc066e314103c08b447ffa7b3327e6223c4ff8fe
Luka Čehovin <luka.cehovin@gmail.com>
2013-07-01 00:28:19 +0200

    Fixing run_trial function.

--- evaluation/experiment/run_trial.m
+++ evaluation/experiment/run_trial.m
@@ -19,9 +19,11 @@ total_frames = 0;
 
 trajectory = zeros(sequence.length, 4);
 
+trajectory(:, 1:3) = NaN;
+
 while start < sequence.length
 
-    [Tr, Tm] = run_tracker(tracker, sequence, start, context);
+    [Tr, Tm] = tracker.run(tracker, sequence, start, context);
 
     if isempty(Tr)
         trajectory = [];
@@ -37,16 +39,26 @@ while start < sequence.length
     failures = find(overlap' < 0.1);
     failures = failures(failures > 1);
 
-    trajectory(start:min(sequence.length - 1, size(Tr, 1) + start), :) = ...
-            Tr(1:min(sequence.length - start, size(Tr, 1)), :);
-
+    trajectory(start, 4) = -1;
+        
     if ~isempty(failures)
+
         first_failure = failures(1) + start - 1;
-        trajectory(first_failure, :) = [0, 0, -1, -1];
+        
+        trajectory(start + 1:min(first_failure, size(Tr, 1) + start - 1), :) = ...
+            Tr(2:min(first_failure - start + 1, size(Tr, 1)), :);
+
+        trajectory(first_failure, 4) = -2;
         start = first_failure + track_properties.failshift;
         print_debug(['INFO: Detected failure at frame ', num2str(first_failure), '. Reinitializing.']);
 
     else
+        
+        if size(Tr, 1) > 1
+            trajectory(start + 1:min(sequence.length, size(Tr, 1) + start - 1), :) = ...
+                Tr(2:min(sequence.length - start + 1, size(Tr, 1)), :);
+        end;
+        
         start = sequence.length;
     end;
 
--- evaluation/tracker/create_tracker.m
+++ evaluation/tracker/create_tracker.m
@@ -3,3 +3,5 @@ function [tracker] = create_tracker(identifier, command, result_directory)
 mkpath(result_directory);
 
 tracker = struct('identifier', identifier, 'command', command, 'directory', result_directory);
+
+tracker.run = @run_tracker;
