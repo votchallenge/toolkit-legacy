1946c58178ad63d440a37de3a7d8f1694ef5ecf3
Luka Čehovin <luka.cehovin@gmail.com>
2013-07-01 22:57:03 +0200

    Build file fix.

--- examples/c/build.sh
+++ examples/c/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc camshift.c -g -I/usr/include/opencv -lopencv_core -lopencv_video -lopencv_imgproc -lopencv_highgui -o camshift
+gcc camshift.c -lm -g -I/usr/include/opencv -lopencv_core -lopencv_video -lopencv_imgproc -lopencv_highgui -o camshift
 
 gcc static.c -g -o static
