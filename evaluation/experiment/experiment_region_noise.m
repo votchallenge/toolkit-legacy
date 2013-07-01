f06f1d175b6677f2302202237ee7308518decc8f
Luka Čehovin <luka.cehovin@gmail.com>
2013-07-01 22:56:37 +0200

    Adding predefined noise.

--- evaluation/experiment/experiment_region_noise.m
+++ evaluation/experiment/experiment_region_noise.m
@@ -5,8 +5,10 @@ global track_properties;
 noise_file = fullfile(fileparts(mfilename('fullpath')), 'noise.txt');
 
 if exist(noise_file, 'file')
+        print_debug('Reading noise from noise.txt ...');
     noise = csvread(noise_file);
 else
+    print_debug('WARNING: noise.txt not found! Generating random noise ...');
     noise = generate_noise(track_properties.repeat * length(sequences));
 end;
 
