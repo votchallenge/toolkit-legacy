ec0a4406258f715a44668d70f4a243b698e8220f
Luka Čehovin <luka.cehovin@gmail.com>
2013-07-03 20:07:47 +0200

    Additional automatic collection of platform information.

--- evaluation/tracker/write_manifest.m
+++ evaluation/tracker/write_manifest.m
@@ -6,6 +6,14 @@ manifest = fullfile(tracker.directory, 'manifest.txt');
 
 [platform_str,platform_maxsize,platform_endian] = computer();
 
+if is_octave()
+    environment = 'octave';
+else
+    environment = 'matlab';
+end;
+
+environment_version = version();
+
 fid = fopen(manifest, 'w');
 
 fprintf(fid, 'toolkit.version=%d\n', toolkit_information());
@@ -14,6 +22,7 @@ fprintf(fid, 'timestamp=%s\n', datestr(now, 31));
 fprintf(fid, 'platfrom=%s\n', platform_str);
 fprintf(fid, 'platfrom.maxsize=%f\n', platform_maxsize);
 fprintf(fid, 'platfrom.endian=%s\n', platform_endian);
-
+fprintf(fid, 'environment=%s\n', environment);
+fprintf(fid, 'environment.version=%f\n', environment_version);
 
 fclose(fid);
