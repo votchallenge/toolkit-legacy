c04d5871be2faa51b20833d34c7a2a1ffb41c39c
Luka Čehovin <luka.cehovin@gmail.com>
2013-09-04 14:19:43 +0200

    Results LaTeX table export.

--- evaluation/tracker/write_report.m
+++ evaluation/tracker/write_report.m
@@ -4,23 +4,35 @@ global track_properties;
 
 report_file = fullfile(track_properties.directory, sprintf('%s-%s.html', tracker.identifier, datestr(now, 30)));
 
+table_file = fullfile(track_properties.directory, sprintf('%s-%s.tex', tracker.identifier, datestr(now, 30)));
+
 sequence_names = cellfun(@(x) x.name, sequences,'UniformOutput',false);
 
 temporary_dir = tempdir;
 
 temporary_file = fullfile(temporary_dir, 'tables.tmp');
 
-fid = fopen(temporary_file, 'w');
+html_fid = fopen(temporary_file, 'w');
+
+latex_fid = fopen(table_file, 'w');
 
 for e = 1:length(experiments)
     
-    fprintf(fid, '<h2>Experiment <em>%s</em></h2>\n', experiments{e});
-    
-    matrix2html(scores{e}', fid, 'rowLabels', {'accuracy', 'robustness', 'speed (fps)'}, 'columnLabels', sequence_names);
-    
+    fprintf(html_fid, '<h2>Experiment <em>%s</em></h2>\n', experiments{e});
+
+    fprintf(latex_fid, '\\begin{table}[h]\\caption{Experiment %s}\\label{tab:results-%s}\n', strrep(experiments{e}, '_', '\_'), strrep(experiments{e}, '_', '-'));
+
+    matrix2html(scores{e}, html_fid, 'columnLabels', {'accuracy', 'robustness', 'speed (fps)'}, 'rowLabels', sequence_names);
+
+    matrix2latex(scores{e}, latex_fid, 'columnLabels', {'accuracy', 'robustness', 'speed (fps)'}, 'rowLabels', strrep(sequence_names, '_', '\_'), 'format', '%.2f');
+
+    fprintf(latex_fid, '\\end{table}\n');
+
 end;
 
-fclose(fid);
+fclose(html_fid);
+
+fclose(latex_fid);
 
 template = fileread(fullfile(fileparts(mfilename('fullpath')), 'template.html'));
 
