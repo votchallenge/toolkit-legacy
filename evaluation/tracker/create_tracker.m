3e44acfee069ecd4e2a8eafaf7f61453a1d1ef94
Luka Čehovin <luka.cehovin@gmail.com>
2013-09-20 13:52:49 +0200

    Changed tracker creation to make the analysis a bit easier.

--- evaluation/tracker/create_tracker.m
+++ evaluation/tracker/create_tracker.m
@@ -1,14 +1,17 @@
 function [tracker] = create_tracker(identifier, result_directory)
 
+mkpath(result_directory);
+
 if exist(['tracker_' , identifier]) ~= 2
-    error('Configuration for tracker %s does not exist.', identifier);
+    print_debug('WARNING: No configuration for tracker %s found', identifier);
+    tracker = struct('identifier', identifier, 'command', [], ...
+        'directory', result_directory, 'linkpath', []);
+    return;
+    %error('Configuration for tracker %s does not exist.', identifier);
 end;
 
 tracker_configuration = str2func(['tracker_' , identifier]);
 tracker_configuration();
-
-
-mkpath(result_directory);
 
 tracker = struct('identifier', identifier, 'command', tracker_command, ...
         'directory', result_directory, 'linkpath', {tracker_linkpath});
