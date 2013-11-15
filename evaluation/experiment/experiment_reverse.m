39d225762788ff597cc1c43f857290583bca0ad1
Luka Čehovin <luka.cehovin@gmail.com>
2013-11-15 17:06:30 +0100

    Fixing reverse experiment

--- evaluation/experiment/experiment_reverse.m
+++ evaluation/experiment/experiment_reverse.m
@@ -2,13 +2,11 @@ function scores = experiment_reverse(tracker, sequences, directory)
 
 global track_properties;
 
+sequences = cellfun(@(x) sequence_reverse(x), sequences,'UniformOutput',false);
+
 for i = 1:length(sequences)
     print_text('Sequence "%s" (%d/%d)', sequences{i}.name, i, length(sequences));
-    sequence = sequences{i};
-    sequence.groundtruth = sequence.groundtruth(end:-1:1, :);
-    sequence.images = sequence.images(end:-1:1, :);
-    sequence.labels.data = sequence.labels.data(end:-1:1, :);
-    repeat_trial(tracker, sequence, track_properties.repeat, fullfile(directory, sequences{i}.name));
+    repeat_trial(tracker, sequences{i}, track_properties.repeat, fullfile(directory, sequences{i}.name));
 end;
 
 scores = calculate_scores(tracker, sequences, directory);
@@ -16,3 +14,14 @@ scores = calculate_scores(tracker, sequences, directory);
 print_text('Experiment complete.');
 
 print_scores(sequences, scores);
+
+end
+
+function reversed_sequence = sequence_reverse(sequence)
+
+	reversed_sequence = sequence;
+    reversed_sequence.groundtruth = sequence.groundtruth(end:-1:1, :);
+    reversed_sequence.images = sequence.images(end:-1:1, :);
+    reversed_sequence.labels.data = sequence.labels.data(end:-1:1, :);
+
+end
