a468a35b609e588164f469f7b3fd3fa3b3ff03d7
Luka Čehovin <luka.cehovin@gmail.com>
2014-02-26 13:50:01 +0100

    Changes to the readfile.

--- evaluation/utilities/readfile.m
+++ evaluation/utilities/readfile.m
@@ -1,14 +1,12 @@
-function varargout = readfile(fname, delim)
+function [M] = readfile(fname, delim)
 
     % Initialize the variable output argument
-    varargout = cell(nargout, 1);
-
-	% Initialize elements of the cell array to nested cell arrays
-	% This syntax is due to {:} producing a comma-separated 
-	[varargout{:}] = deal(cell(0, 0));
+    M = cell(0, 0);
 
 	fid = fopen(fname, 'r');
 
+    lni = 0;
+    
 	while true
 	    % Get the current line
 	    ln = fgetl(fid);
@@ -16,8 +14,10 @@ function varargout = readfile(fname, delim)
 	    % Stop if EOF
 	    if ln == -1
 	        break;
-	    end
+        end
 
+        lni = lni + 1;
+        
 	    % Split the line string into components and parse numbers
 		% elems = strsplit(ln, delim);
 		p = strfind(ln, delim);
@@ -45,16 +45,24 @@ function varargout = readfile(fname, delim)
 	    % (i.e. the indices of the strings in the original data)
 	    idxnans = find(nans);
 
+        for i = 1:length(elms)
+ 	        if any(ismember(idxnans, i))
+ 	            M{lni, i} = elms{i};
+ 	        else
+ 	            M{lni, i} = nums(i);
+ 	        end
+        end;
+        
 	    % Assign each corresponding element in the current line
 	    % into the corresponding cell array of varargout
-	    for i = 1:nargout
-	        % Detect if the current index is a string or a num
-	        if any(ismember(idxnans, i))
-	            varargout{i}{end+1} = elms{i};
-	        else
-	            varargout{i}{end+1} = nums(i);
-	        end
-	    end
+% 	    for i = 1:nargout
+% 	        % Detect if the current index is a string or a num
+% 	        if any(ismember(idxnans, i))
+% 	            varargout{i}{end+1} = elms{i};
+% 	        else
+% 	            varargout{i}{end+1} = nums(i);
+% 	        end
+% 	    end
 	end;
 
 end
