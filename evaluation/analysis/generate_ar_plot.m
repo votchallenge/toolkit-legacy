b32e229af20a9335b27eaec5b0e70495b1425a6a
Luka Čehovin <luka.cehovin@gmail.com>
2014-02-26 13:50:31 +0100

    Additional parameters for AR plot function.

--- evaluation/analysis/generate_ar_plot.m
+++ evaluation/analysis/generate_ar_plot.m
@@ -6,6 +6,12 @@ function hf = generate_ar_plot(trackers, accuracy, robustness, varargin)
     width = 6;
     height = 4;
 
+    hf = [];
+    
+    plot_callback = @default_plot_ar;
+    
+    show_legend = true;
+    
     for i = 1:2:length(varargin)
         switch lower(varargin{i})
             case 'title'
@@ -18,16 +24,26 @@ function hf = generate_ar_plot(trackers, accuracy, robustness, varargin)
                 width = varargin{i+1};
             case 'height'
                 height = varargin{i+1};
+            case 'callback'
+                plot_callback = varargin{i+1};
+            case 'handle'
+                hf = varargin{i+1};
+            case 'legend'
+                show_legend = varargin{i+1};                    
             otherwise 
                 error(['Unknown switch ', varargin{i},'!']) ;
         end
     end 
 
-    if ~visible
-        hf = figure('Visible', 'off');
+    if isempty(hf)
+        if ~visible
+            hf = figure('Visible', 'off');
+        else
+            hf = figure();
+        end
     else
-        hf = figure();
-    end
+        figure(hf);
+    end;
 
     hold on;
     grid on;
@@ -39,22 +55,34 @@ function hf = generate_ar_plot(trackers, accuracy, robustness, varargin)
 
     for t = 1:length(trackers)
 
-        if all(isnan(accuracy(:, t)))
+        if all(isnan(accuracy(:, t, :)))
             available(t) = 0;
             continue;
         end;
 
-        ar_mean = mean([accuracy(:, t), robustness(:, t)], 1);
-
-        plot(exp(-ar_mean(2) * sensitivity), ar_mean(1), ...
-            trackers{t}.style.symbol, 'Color', trackers{t}.style.color, ...
-            'MarkerSize', 10, 'LineWidth', trackers{t}.style.width);
-
+        ar_mean = squeeze(mean([accuracy(:, t, :), robustness(:, t, :)], 1));
+        
+        if size(ar_mean, 1) == 1
+            plot_robustness = exp(-ar_mean(2) * sensitivity);
+            plot_accuracy = ar_mean(1);
+        else
+            plot_robustness = exp(-ar_mean(2, :) * sensitivity);
+            plot_accuracy = ar_mean(1, :);   
+            
+        end;
+        
+        plot_callback(plot_robustness, plot_accuracy, ...
+                trackers{t}.style.symbol, trackers{t}.style.color, ...
+                trackers{t}.style.width);
+        
     end;
     
     tracker_labels = cellfun(@(x) x.label, trackers, 'UniformOutput', 0);
 
-    legend(tracker_labels(available), 'Location', 'NorthWestOutside'); 
+    if show_legend
+        legend(tracker_labels(available), 'Location', 'NorthWestOutside'); 
+    end;
+    
     xlabel(sprintf('Reliability (S = %d)', sensitivity));
     ylabel('Accuracy');
     xlim([0, 1]); 
@@ -63,3 +91,12 @@ function hf = generate_ar_plot(trackers, accuracy, robustness, varargin)
     set(hf, 'PaperUnits', 'inches', 'PaperSize', [width, height], 'PaperPosition', [0, 0, width, height]);
 
     hold off;
+    
+end
+
+function default_plot_ar(robustness, accuracy, symbol, color, width)
+
+    plot(robustness, accuracy, symbol, 'Color', color, ...
+        'MarkerSize', 10, 'LineWidth', width);
+
+end
