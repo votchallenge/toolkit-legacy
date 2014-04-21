1573c4810155d4e494e18fe4641e2d8c659b4c92
Luka Čehovin <luka.cehovin@gmail.com>
2014-04-21 22:21:25 +0200

    Initial documentation generation script.

---
+++ documentation/README.md
@@ -0,0 +1,3 @@
+The official VOT toolkit documentation has been migrated to Github wiki of the project and is available [https://github.com/vicoslab/vot-toolkit/wiki](here).
+
+This folder contains scripts that can be used to build an offline PDF version of the documentation from the online version.
---
+++ documentation/generate_pdf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+WIKI=./vot-toolkit.wiki
+FILES=$WIKI/Home.md $WIKI/Integration.md $WIKI/Internals.md
+
+sed -s -e '$a\\' $FILES | sed "s/\[\[\([a-zA-Z 0-9]*\)|\([a-zA-Z 0-9]*\)\]\] document/\1 chapter/g" | sed "s/\[\[\([a-zA-Z 0-9]*\)|\([a-zA-Z 0-9]*\)\]\]/\1/g" | pandoc --include-in-header "header.tex" --include-before-body="cover.tex" --standalone --listings --chapters --toc -f markdown -t latex -o documentation.tex 
+
+pdflatex documentation.tex
+
