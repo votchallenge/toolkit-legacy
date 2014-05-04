20f5bcf06d7b0945d0779d13f3ae226fe7b4fd04
Luka Čehovin <luka.cehovin@gmail.com>
2014-05-04 20:07:03 +0200

    Caching for mex compilation.

--- utilities/compile_mex.m
+++ utilities/compile_mex.m
@@ -1,48 +1,67 @@
 function [success] = compile_mex(name, files, includes, directory)
 
-if exist(name, 'file') == 3
-     success = true;
-     return;
-end
-
-arguments = {};
-
-if nargin < 3
-	includes = cell(0);
-end
-
-if nargin < 4
-    directory = '';
-end;
-
-includes = cellfun(@(x) sprintf('-I%s', x), includes, 'UniformOutput', false);
-
-old_dir = pwd;
-
-try
-
-    if ~isempty(directory)
-        cd(directory)
-    end;
-
-    if is_octave() 
-
-        mkoctfile('-mex', '-o', name, includes{:}, files{:}, arguments{:});
-
-    else
-
-        mex('-output', name, includes{:}, files{:}, arguments{:});
-
+    function datenum = file_timestamp(filename)
+        if ~exist(filename, 'file')
+            datenum = 0;
+            return;
+        end;
+        file_description = dir(filename);
+        datenum = file_description.datenum;
     end
 
-    cd(old_dir);
-    
-    success = true;
+    if exist(name, 'file') == 3
 
-catch e
-    
-    cd(old_dir);
-    print_text('ERROR: Unable to compile MEX function: "%s".', e.message);
-    success = false;
+        function_timestamp = file_timestamp(which(name));
 
+        older = cellfun(@(x) file_timestamp(x) < function_timestamp, files, 'UniformOutput', true);
+
+        if all(older)
+        
+            success = true;
+            return;
+        end;
+    end
+
+    arguments = {};
+
+    if nargin < 3
+        includes = cell(0);
+    end
+
+    if nargin < 4
+        directory = '';
+    end;
+
+    includes = cellfun(@(x) sprintf('-I%s', x), includes, 'UniformOutput', false);
+
+    old_dir = pwd;
+
+    try
+
+        if ~isempty(directory)
+            cd(directory)
+        end;
+
+        if is_octave() 
+
+            mkoctfile('-mex', '-o', name, includes{:}, files{:}, arguments{:});
+
+        else
+
+            mex('-output', name, includes{:}, files{:}, arguments{:});
+
+        end
+
+        cd(old_dir);
+
+        success = true;
+
+    catch e
+
+        cd(old_dir);
+        print_text('ERROR: Unable to compile MEX function: "%s".', e.message);
+        success = false;
+
+    end
+    
 end
