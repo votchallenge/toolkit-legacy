bd6382116403533f76a90de9dfb70912e6f1ed89
Luka Čehovin <luka.cehovin@gmail.com>
2014-05-06 09:31:56 +0200

    Updating VOT2013 stack with more general region_noise experiment implementation.

--- experiment/stack_vot2013.m
+++ experiment/stack_vot2013.m
@@ -10,7 +10,8 @@ baseline.converter = [];
 baseline.execution = 'default';
 
 region_noise.name = 'region_noise';
-region_noise.converter = 'sequence_noisy_initialization';
+region_noise.converter = @(sequence) sequence_transform_initialization(...
+    sequence, @noisy_transform);
 region_noise.execution = 'default';
 
 grayscale.name = 'grayscale';
@@ -19,3 +20,15 @@ grayscale.execution = 'default';
 
 experiments = {baseline, region_noise, grayscale};
 
+end
+
+function [transform] = noisy_transform(sequence, index, context)
+
+    bounds = region_convert(get_region(sequence, index), 'rectangle');
+
+    scale = 0.9 + rand(1, 2) * 0.2;
+    move = bounds(3:4) .* (0.1 - rand(1, 2) * 0.2);
+
+    transform = [scale(1), 0, move(1); 0, scale(2), move(2); 0, 0, 1];
+
+end
