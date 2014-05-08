6b89c753925ed99a6c2c8a37d4df029ecedadd1f
Luka Čehovin <luka.cehovin@gmail.com>
2014-05-08 21:16:47 +0200

    Reversing region_offset offset

--- sequence/region_offset.m
+++ sequence/region_offset.m
@@ -3,11 +3,11 @@ function [region] = region_offset(region, offset)
 if isnumeric(region) 
 	if numel(region) == 4
 
-        region(1:2) = region(1:2) - offset;
+        region(1:2) = region(1:2) + offset;
 
     elseif numel(region) >= 6 && mod(numel(region), 2) == 0
 
-        region = region - repmat(offset(:)', 1, numel(region) / 2);  
+        region = region + repmat(offset(:)', 1, numel(region) / 2);  
         
 	end;
         
