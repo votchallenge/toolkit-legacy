4591032de94b21f8ac23b01618e953c9f5e79c9a
Luka Čehovin <luka.cehovin@gmail.com>
2014-05-29 10:22:39 +0200

    Fixing Matlab NCC tracker implementation.

--- tracker/examples/matlab/tracker_ncc_initialize.m
+++ tracker/examples/matlab/tracker_ncc_initialize.m
@@ -4,6 +4,8 @@ gray = double(rgb2gray(I));
 
 [height, width] = size(gray);
 
+region = round(region);
+
 % If the provided region is a polygon ...
 if numel(region) > 4
 	x1 = min(region(1:2:end));
@@ -14,17 +16,16 @@ if numel(region) > 4
 	region = [x1, y1, x2 - x1, y2 - y1];
 end;
 
-x1 = max(1, region(1));
-y1 = max(1, region(2));
-x2 = min(width, region(1) + region(3));
-y2 = min(height, region(2) + region(4));
+x1 = max(0, region(1));
+y1 = max(0, region(2));
+x2 = min(width-1, region(1) + region(3) - 1);
+y2 = min(height-1, region(2) + region(4) - 1);
  
-template = gray(y1:y2, x1:x2);
+template = gray((y1:y2)+1, (x1:x2)+1);
 
-position = [x1 + x2, y1 + y2] / 2;
+state = struct('template', template, 'size', [x2 - x1 + 1, y2 - y1 + 1]);
+state.window = max(state.size) * 2;
+state.position = [x1 + x2 + 1, y1 + y2 + 1] / 2;
 
-state = struct('template', template, 'position', position, ...
-    'window', max([x2 - x1, y2 - y1]) * 2, 'size', [x2 - x1, y2 - y1] ) ;
-    
-location = [x1, y1, x2 - x1, y2 - y1];
+location = [x1, y1, state.size];
 
--- tracker/examples/matlab/tracker_ncc_update.m
+++ tracker/examples/matlab/tracker_ncc_update.m
@@ -4,12 +4,14 @@ gray = double(rgb2gray(I)) ;
 
 [height, width] = size(gray);
 
-x1 = max(1, round(state.position(1) - state.window / 2));
-y1 = max(1, round(state.position(2) - state.window / 2));
-x2 = min(width, round(state.position(1) + state.window / 2));
-y2 = min(height, round(state.position(2) + state.window / 2));
+x1 = max(0, round(state.position(1) - state.window / 2));
+y1 = max(0, round(state.position(2) - state.window / 2));
+x2 = min(width-1, round(state.position(1) + state.window / 2));
+y2 = min(height-1, round(state.position(2) + state.window / 2));
 
-region = gray(y1:y2, x1:x2);
+region = gray((y1:y2)+1, (x1:x2)+1);
+
+%[x1 y1 x2 y2 state.position]
 
 if any(size(region) < size(state.template))
     location = [state.position - state.size / 2, state.size];
@@ -18,10 +20,18 @@ end;
 
 C = normxcorr2(state.template, region);
 
+% We are only using valid part of the response (where full template is used)
+pad = size(state.template) - 1;
+center = size(region) - pad - 1;
+C = C([false(1,pad(1)) true(1,center(1))], [false(1,pad(2)) true(1,center(2))]);
+
+x1 = x1 + pad(2);
+y1 = y1 + pad(1);
+imwrite((C - min(C(:))) ./ (max(C(:)) - min(C(:))), 'matlab.png');
 [~, imax] = max(C(:));
 [my, mx] = ind2sub(size(C),imax(1));
 
-position = [x1 + mx - 1 - state.size(1) / 2, y1 + my - 1 - state.size(2) / 2];
+position = [x1 + mx - state.size(1) / 2, y1 + my - state.size(2) / 2];
 
 state.position = position;
 location = [position - state.size / 2, state.size];
