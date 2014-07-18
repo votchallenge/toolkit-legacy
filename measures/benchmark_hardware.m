7e38292adc5469cd971b4ba923bd10f646300768
Luka Čehovin <luka.cehovin@gmail.com>
2014-07-18 14:38:35 +0200

    Matlab startup benchmark works on Windows too.

--- measures/benchmark_hardware.m
+++ measures/benchmark_hardware.m
@@ -77,35 +77,67 @@ performance.nonlinear_native = toc / repetitions;
 if ~is_octave()
 	print_debug('Performing Matlab startup time benchmark');
 
+	time_file = fullfile(pwd, 'time.txt');
+
 	if isunix
-		matlab_executable = [fullfile(matlabroot, 'bin', 'matlab'), ' -nodesktop -nosplash -r quit'];
+		matlab_executable = [fullfile(strrep(matlabroot, ' ', '\ '), 'bin', 'matlab'), ' -nodesktop -nosplash -r "dlmwrite(''', time_file, ''', now, ''precision'', 30); quit;"'];
 	else
-		matlab_executable = [fullfile(matlabroot, 'bin', 'matlab.exe'), ' -nodesktop -nosplash -wait -minimize -r quit'];
+		matlab_executable = ['"', fullfile(matlabroot, 'bin', 'matlab.exe'), '" -nodesktop -nosplash -wait -minimize -r "dlmwrite(''', time_file, ''', now, ''precision'', 30); quit;"'];
 	end
 
 	try
 
 		repetitions = 3;
 
-		tic;
-
+		totaltime = 0;
+        
 		for i = 1:repetitions
 
+            if exist(time_file, 'file')
+                delete(time_file);
+            end;
+
+            startime = now;
+
 			if verLessThan('matlab', '7.14.0')
-				[status, output] = system(matlab_executable);
+				[result, output] = system(matlab_executable);
 			else
-				[status, output] = system(matlab_executable, '');
+				[result, output] = system(matlab_executable, '');
 			end;
 
+            endtime = NaN;
+            
+            for w = 1:500
+      
+                if exist(time_file, 'file')
+                    endtime = csvread(time_file);
+                    delete(time_file);
+					break;
+                end;
+                pause(0.1);
+            end;
+            
+            if isnan(endtime)
+                print_debug('ERROR: Unable to time Matlab: timeout.');
+            end;
+
+            totaltime = totaltime + (endtime - startime) * 86400;
+            
 		end;
 
-		performance.matlab_startup = toc / repetitions;
+		performance.matlab_startup = totaltime / repetitions;
 
 	catch e
 
 		print_debug('ERROR: Exception thrown "%s".', e.message);
 	end;
 end
+
+[platform_str,platform_maxsize,platform_endian] = computer();
+
+performance.platform = platform_str;
+performance.platform_maxsize = platform_maxsize;
+performance.platform_endian = platform_endian;
 
 writestruct(filename, performance);
 
