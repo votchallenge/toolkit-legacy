2502952c413ce8420ac110df7b11541df9501a8e
novotl22 <novotl22@fel.cvut.cz>
2014-08-07 17:18:53 +0200

    Taking back unnecessary changes

--- measures/estimate_failures.m
+++ measures/estimate_failures.m
@@ -1,12 +1,5 @@
-function [failures] = estimate_failures(trajectory, sequence)
+function [count, failures] = estimate_failures(trajectory, sequence)
 
-stack = get_global_variable('stack', 'vot2014');
+failures = find(cellfun(@(x) numel(x) == 1 && x == 2, trajectory, 'UniformOutput', true));
 
-if strcmp('vot2013', stack)    
-    failures = sum(cellfun(@(x) numel(x) == 1 || x(4) == -2, trajectory, 'UniformOutput', true));    
-else
-    failures = sum(cellfun(@(x) numel(x) == 1 && x == 2, trajectory, 'UniformOutput', true));
-end
-
-
-
+count = numel(failures);
