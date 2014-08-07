f77571da5be2cee3f041a2f9643c06cbca7e9953
novotl22 <novotl22@fel.cvut.cz>
2014-08-07 11:07:51 +0200

    Fix estimates for results from 2013

--- measures/estimate_accuracy.m
+++ measures/estimate_accuracy.m
@@ -12,7 +12,13 @@ end
 
 if burnin > 0
     
-    mask = cellfun(@(r) numel(r) == 1 && r == 1, trajectory, 'UniformOutput', true);
+    stack = get_global_variable('stack', 'vot2013');
+    
+    if strcmp('vot2013', stack)    
+        mask = cellfun(@(r) numel(r) == 1 || r(4) == -1, trajectory, 'UniformOutput', true);    
+    else
+        mask = cellfun(@(r) numel(r) == 1 && r == 1, trajectory, 'UniformOutput', true);
+    end
 
     if is_octave()
         se = logical([zeros(burnin - 1, 1); ones(burnin, 1)]);
--- measures/estimate_failures.m
+++ measures/estimate_failures.m
@@ -1,6 +1,12 @@
-function [count, failures] = estimate_failures(trajectory, sequence)
+function [failures] = estimate_failures(trajectory, sequence)
 
-failures = find(cellfun(@(x) numel(x) == 1 && x == 2, trajectory, 'UniformOutput', true));
+stack = get_global_variable('stack', 'vot2014');
 
-count = numel(failures);
+if strcmp('vot2013', stack)    
+    failures = sum(cellfun(@(x) numel(x) == 1 || x(4) == -2, trajectory, 'UniformOutput', true));    
+else
+    failures = sum(cellfun(@(x) numel(x) == 1 && x == 2, trajectory, 'UniformOutput', true));
+end
+
+
 
