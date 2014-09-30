30f271901cfd98aede1492e0e3869f0344a2b3e0
Luka Čehovin <luka.cehovin@gmail.com>
2014-09-30 16:17:32 +0200

    Support for LaTeX export in reports.

--- report/create_document.m
+++ report/create_document.m
@@ -12,6 +12,7 @@ for i = 1:2:length(varargin)
 end 
 
 temporary_file = tempname;
+temporary_metadata = [tempname, '.mat'];
 temporary_fid = fopen(temporary_file, 'w');
 
 template_file = get_global_variable('report_template');
@@ -20,9 +21,14 @@ target_file = fullfile(context.root, sprintf('%s%s.html', context.prefix, name))
 
 document = struct('fid', temporary_fid, ...
     'temporary_file', temporary_file, 'title', title, ...
-    'template_file', template_file, 'target_file', target_file);
+    'template_file', template_file, 'target_file', target_file, ...
+    'temporary_metadata', temporary_metadata);
 
 document.url = sprintf('%s%s.html', context.prefix, name);
+
+document.raw = @(text, varargin) fprintf(document.fid, text, varargin{:});
+
+document.include = @(type, name) include_resource(context, document, type, name);
 
 document.write = @() write_report_document(document);
 
@@ -34,13 +40,12 @@ document.subsection = @(text, varargin) insert_subsection(document, sprintf(text
 
 document.text = @(text, varargin) insert_text(document, sprintf(text, varargin{:}));
 
-document.table = @(data, varargin) insert_table(document, data, varargin{:});
+document.table = @(data, varargin) insert_table(context, document, data, varargin{:});
 
 document.link = @(url, text, varargin) insert_link(document, url, sprintf(text, varargin{:}));
 
 document.figure = @(handle, id, title) insert_figure(context, document.fid, handle, id, title);
 
-document.raw = @(text, varargin) fprintf(document.fid, text, varargin{:});
 
 end
 
@@ -48,11 +53,33 @@ function write_report_document(document)
 
     fclose(document.fid);
 
+    metadata = struct();
+
+    load(document.temporary_metadata);
+
+    resources = struct2cell(metadata.resources);
+    tokens = cell(numel(resources), 1);
+    
+    for i = 1:numel(resources)
+        url = sprintf('%s/%s/%s', 'resources', resources{i}.type, resources{i}.name);
+
+        switch resources{i}.type
+            case 'js'
+                tokens{i} = sprintf('<script type="text/javascript" src="%s"></script>\n', url);
+            case 'css'
+                tokens{i} = sprintf('<link rel="stylesheet" type="text/css" href="%s"/>\n', url);
+            otherwise
+                tokens{i} = '';   
+        end;
+
+    end;
+
     generate_from_template(document.target_file, document.template_file, ...
         'body', fileread(document.temporary_file), 'title', document.title, ...
-        'timestamp', datestr(now, 31));
+        'timestamp', datestr(now, 31), 'head', strjoin(tokens, ''));
     
     delete(document.temporary_file);
+    delete(document.temporary_metadata);
     
 end
 
@@ -87,9 +114,20 @@ function insert_link(document, url, text)
 
 end
 
-function insert_table(document, data, varargin)
+function insert_table(context, document, data, varargin)
 
-    fprintf(document.fid, '<div class="table">');
+    if context.exportlatex
+    
+        document.include('js', 'jquery.js');
+        document.include('js', 'jquery.latex.js');
+
+        fprintf(document.fid, '<div class="table latex">');
+        
+    else
+        
+        fprintf(document.fid, '<div class="table">');
+        
+    end;
 
     matrix2html(data, document.fid, varargin{:});
 
@@ -97,4 +135,35 @@ function insert_table(document, data, varargin)
 
 end
 
+function include_resource(context, document, type, name)
+
+    resource_destination = fullfile(context.root, 'resources', type, name);
+    resource_source = fullfile(get_global_variable('toolkit_path'), 'report', 'resources', type, name);
+
+    if ~exist(resource_source, 'file')
+        return;
+    end;
+
+    if ~exist(resource_destination, 'file') || ...
+        file_newer_than(resource_source, resource_destination)
+        mkpath(fullfile(context.root, 'resources', type));
+        copyfile(resource_source, resource_destination);
+    end;
+
+
+    metadata = struct('resources', struct());
+
+    if exist(document.temporary_metadata, 'file')
+        load(document.temporary_metadata);
+    end;
+
+    resource_id = sprintf('%s_%s', type, name);
+    resource_id = strrep(strrep(strrep(resource_id, '/', '_'), '.', '_'), '-', '_');
+    
+    if ~isfield(metadata.resources, resource_id)
+        metadata.resources.(resource_id) = struct('type', type, 'name', name);
+        save(document.temporary_metadata, 'metadata');
+    end;
+
+end
 
---
+++ report/resources/js/jquery.latex.js
@@ -0,0 +1,105 @@
+LaTeX = new Object();
+
+LaTeX.escape = function(s) {
+
+	return s.replace("\\", "\\textbackslash").replace("~", "\\textasciitilde").replace("&", "\\&").replace("_", "\\_");
+
+}
+
+$(function () {
+
+    function tableToLaTeX(table) {
+
+        var tableColumns = 0;
+
+        var tableData = [];
+
+        $.each($(table).find('tr'), function(i, row) {
+
+            var columns = 0;
+
+            var rowData = [];
+
+            $.each($(row).children(), function(i, cell) {
+                cell = $(cell);
+                var colspan = parseInt(cell.attr('colspan'));
+                var rowspan = parseInt(cell.attr('rowspan'));
+
+                if (!colspan) colspan = 1;
+                if (!rowspan) rowspan = 1;
+
+                rowData.push({'header' : cell.is('th'), 'text' : cell.text(), 'rows' : rowspan, 'columns': colspan});
+                columns += colspan;
+
+            });
+
+            tableData.push(rowData);
+
+            tableColumns = Math.max(columns, tableColumns);
+        });
+
+        text = '\\begin{tabular}{|' + Array(tableColumns + 1).join("c|") + '}\n';
+
+        text += '\\hline\n';
+
+        $.each(tableData, function(i, rowData) {
+
+            var renderedRow = [];
+
+            $.each(rowData, function(i, cell) {
+
+                var rendered = LaTeX.escape(cell.text);
+
+                if (cell.header) {
+                    rendered = '\\textbf{' + rendered + '}';
+                }
+
+                if (cell.rows > 1) {
+                    rendered = '\\multirow{' + cell.rows + '}{*}{' + rendered + '}';
+                }
+
+                if (cell.columns > 1) {
+                    rendered = '\\multicolumn{' + cell.columns + '}{ c }{' + rendered + '}';
+                }
+
+                renderedRow.push(rendered);
+
+            });
+
+            text += renderedRow.join(' & ') + ' \\\\\\hline\n';
+
+        });
+
+
+        text += '\\end{tabular}\n';
+
+        return text;
+    }
+
+
+    $.each($('.latex table'), function (i, table) {
+
+        var converted = $('<pre>').hide();
+
+        var toggle = $('<a>').addClass('latexlink').text('As LaTeX').click(function() {
+
+            if (converted.is(':visible')) {
+                converted.hide();
+                $(table).show();
+                toggle.text('As LaTeX');
+            } else {
+                var data = tableToLaTeX(table);
+                converted.text(data);
+                $(table).hide();
+                converted.show();
+                toggle.text('As HTML');
+            }
+        });
+
+        $(table).after(toggle);
+        $(table).after(converted);
+
+
+    });
+
+});
