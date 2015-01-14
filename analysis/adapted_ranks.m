0fc0bb19acfa126f0f1fa1f2638c066df23ad886
Luka Čehovin <luka.cehovin@gmail.com>
2015-01-14 12:19:23 +0100

    More options for rank adaptation based on statistical tests.

--- analysis/adapted_ranks.m
+++ analysis/adapted_ranks.m
@@ -1,7 +1,36 @@
 function a_ranks = adapted_ranks(ranks, different)
 
+mode = 'reranked';
+
 a_ranks = zeros(1, length(ranks)) ;
 
-for tracker = 1:length(ranks)
-    a_ranks(tracker) = mean(ranks(find(different(tracker,:) == 0))) ; %#ok<FNDSB>
-end 
+switch mode
+
+	case 'mean'
+
+		for tracker = 1:length(ranks)
+			a_ranks(tracker) = mean(ranks(find(~different(tracker,:)))) ; %#ok<FNDSB>
+		end 
+
+	case 'median'
+
+		for tracker = 1:length(ranks)
+			a_ranks(tracker) = median(ranks(find(~different(tracker,:)))) ; %#ok<FNDSB>
+		end 
+
+	case 'best'
+
+		for tracker = 1:length(ranks)
+			a_ranks(tracker) = min(ranks(find(~different(tracker,:)))) ; %#ok<FNDSB>
+		end 
+
+	case 'reranked'
+
+		for tracker = 1:length(ranks)
+            a_ranks(tracker) = min(ranks(find(different(tracker,:) == 0)));  %#ok<FNDSB>
+        end 
+
+        [~, ~, reranked] = unique(a_ranks);
+        a_ranks = reranked; 
+        
+end;
