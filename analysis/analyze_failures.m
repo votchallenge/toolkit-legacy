22df1f0cbc6b5c0aa990443a2b128745004a7420
Luka Čehovin <luka.cehovin@gmail.com>
2015-01-20 17:25:39 +0100

    Updating failure analysis for new report organization.

---
+++ analysis/analyze_failures.m
@@ -0,0 +1,85 @@
+function [failure_histograms] = analyze_failures(experiment, trackers, sequences, varargin)
+
+    cache = fullfile(get_global_variable('directory'), 'cache');
+    
+    for i = 1:2:length(varargin)
+        switch lower(varargin{i})               
+            case 'cache'
+                cache = varargin{i+1};                   
+            otherwise 
+                error(['Unknown switch ', varargin{i},'!']) ;
+        end
+    end 
+
+
+    repeat = get_global_variable('repeat', 1);
+
+    print_text('Failure analysis for experiment %s ...', experiment.name);
+
+    sequences_hash = md5hash(strjoin((cellfun(@(x) x.name, sequences, 'UniformOutput', false)), '-'), 'Char', 'hex');
+    trackers_hash = md5hash(strjoin((cellfun(@(x) x.identifier, trackers, 'UniformOutput', false)), '-'), 'Char', 'hex');
+    
+    mkpath(fullfile(cache, 'failures'));
+    
+    cache_file = fullfile(cache, 'failures', sprintf('%s_%s_%s.mat', experiment.name, trackers_hash, sequences_hash));
+
+    failure_histograms = [];
+	if exist(cache_file, 'file')         
+		load(cache_file);       
+	end;       
+    
+    if ~isempty(failure_histograms)
+        print_text('Loading failure analysis results from cache.');
+        return;
+    end; 
+    
+    experiment_sequences = convert_sequences(sequences, experiment.converter);
+        
+    failure_histograms = cell(1, numel(experiment_sequences));
+    
+    for s = 1:length(experiment_sequences)
+
+        print_indent(1);
+
+        failure_histogram = zeros(numel(trackers), experiment_sequences{s}.length);
+        
+        print_text('Processing sequence %s ...', experiment_sequences{s}.name);
+
+        for t = 1:length(trackers)
+
+            print_indent(1);
+
+            result_directory = fullfile(trackers{t}.directory, experiment.name, experiment_sequences{s}.name);
+            
+            for j = 1:repeat
+
+                result_file = fullfile(result_directory, sprintf('%s_%03d.txt', experiment_sequences{s}.name, j));
+                
+                try 
+                    trajectory = read_trajectory(result_file);
+                catch
+                    continue;
+                end;
+
+                if length(trajectory) < experiment_sequences{s}.length
+                    trajectory{end+1:experiment_sequences{s}.length} = 0;
+                end;
+                
+                [~, failures] = estimate_failures(trajectory, experiment_sequences{s});
+                
+                failure_histogram(t, failures) = failure_histogram(t, failures) + 1;
+
+            end;
+
+            failure_histograms{s} = failure_histogram;
+            
+            print_indent(-1);
+
+        end;
+
+        print_indent(-1);
+        
+    end;
+
+    save(cache_file, 'failure_histograms');
+    
---
+++ report/report_failures.m
@@ -0,0 +1,60 @@
+function [document] = report_failures(context, trackers, sequences, experiments, varargin)
+
+document = create_document(context, 'failures', 'title', 'Failure overview');
+
+for i = 1:2:length(varargin)
+    switch lower(varargin{i})         
+        otherwise 
+            error(['Unknown switch ', varargin{i},'!']) ;
+    end
+end
+
+tracker_labels = cellfun(@(x) iff(isfield(x.metadata, 'verified') && x.metadata.verified, [x.label, '*'], x.label), trackers, 'UniformOutput', 0);
+
+for e = 1:numel(experiments)
+
+    experiment = experiments{e};
+
+    histograms = analyze_failures(experiment, trackers, sequences, 'cache', context.cachedir);
+
+    document.subsection('Experiment %s', experiment.name);
+
+    experiment_sequences = convert_sequences(sequences, experiment.converter);
+            
+    for s = 1:length(experiment_sequences)
+
+        document.subsection('Sequence %s',  experiment_sequences{s}.name);
+        
+        document.raw('<div class="imagegrid">\n');
+        
+        hf = figure('Visible', 'off');
+        imagesc(histograms{s});
+        set(gca,'ytick',(1:numel(trackers)),'yticklabel', tracker_labels);
+        
+        document.figure(hf, sprintf('failures_%s_%s_individual.png', experiment.name, experiment_sequences{s}.name), ...
+            sprintf('Sequence %s, individual trackers', experiment_sequences{s}.name));
+        
+        hf = figure('Visible', 'off');
+        labels = experiment_sequences{s}.labels.data;
+        labelsplit = mat2cell(labels, size(labels, 1), ones(1, size(labels, 2)));
+        starts = cellfun(@(x) find(diff([0; x; 0]) > 0), labelsplit, 'UniformOutput', 0);
+        ends = cellfun(@(x) find(diff([0; x; 0]) < 0), labelsplit, 'UniformOutput', 0);
+
+        hold on;
+        timeline(experiment_sequences{s}.labels.names, starts, ends);
+        combined_histogram = sum(histograms{s}, 1);
+        combined_histogram = (combined_histogram / max(combined_histogram)) * numel(experiment_sequences{s}.labels.names);
+        plot(combined_histogram);
+        set(gca, 'XLim', [1, experiment_sequences{s}.length]);
+        hold off;
+
+        document.figure(hf, sprintf('failures_%s_%s_combined.png', experiment.name, experiment_sequences{s}.name), ...
+            sprintf('Sequence %s, combined failures with properties', experiment_sequences{s}.name));
+        
+        document.raw('</div>\n');
+    end;
+
+end;
+
+document.write();
+
