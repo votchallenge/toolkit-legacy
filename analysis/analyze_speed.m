4a817a4b2b3012f9bdea3847b6998f51af88d937
Luka Čehovin <luka.cehovin@gmail.com>
2015-03-31 11:22:28 +0200

    Speed analysis is using new item iterator.

--- analysis/analyze_speed.m
+++ analysis/analyze_speed.m
@@ -18,6 +18,8 @@ mkpath(fullfile(cache, 'speed'));
 
 cache_file = fullfile(cache, 'speed', sprintf('%s_%s_%s.mat', experiments_hash, trackers_hash, sequences_hash));
 
+print_text('Performing speed analysis ...');
+
 if exist(cache_file, 'file') 
         normalized = [];
         original = [];
@@ -31,32 +33,53 @@ end;
 normalized = nan(length(experiments), length(trackers), length(sequences));
 original = nan(length(experiments), length(trackers), length(sequences));
 
-repeat = get_global_variable('repeat', 1);
+result = struct('normalized', normalized, 'original', original);
 
-for e = 1:numel(experiments)
+result = iterate(experiments, trackers, sequences, 'iterator', @speed_iterator, 'context', result);
 
-    experiment = experiments{e};
+normalized = result.normalized;
+original = result.original;
 
-    print_text('Speed analysis for experiment %s ...', experiment.name);
+save(cache_file, 'normalized', 'original');
 
-    print_indent(1);
+end
 
-	experiment_sequences = convert_sequences(sequences, experiment.converter);
+function context = speed_iterator(event, context)
 
-    for t = 1:length(trackers)
+    switch (event.type)
+        case 'experiment_enter'
+            
+            print_debug('Experiment %s', event.experiment.name);
 
-        directory = fullfile(trackers{t}.directory, experiment.name);
+            print_indent(1);       
+        case 'experiment_exit'
 
-		print_text('Tracker %s ...', trackers{t}.identifier);
+            print_indent(-1);
 
-        for s = 1:length(experiment_sequences)
+        case 'tracker_enter'
+            
+            print_debug('Tracker %s', event.tracker.identifier);
 
+            print_indent(1);  
+            
+        case 'tracker_exit'
+
+            print_indent(-1);
+            
+        case 'sequence_enter'
+            
+            directory = fullfile(event.tracker.directory, event.experiment.name);
+            
+            print_debug('Sequence %s', event.sequence.name);
+
+            repeat = event.experiment.parameters.repeat;
+            
             reliability = nan(repeat, 1);
 			failures = cell(repeat, 1);
 
             for j = 1:repeat
 
-                result_file = fullfile(directory, experiment_sequences{s}.name, sprintf('%s_%03d.txt', experiment_sequences{s}.name, j));
+                result_file = fullfile(directory, event.sequence.name, sprintf('%s_%03d.txt', event.sequence.name, j));
 
                 try 
                     trajectory = read_trajectory(result_file);
@@ -64,57 +87,51 @@ for e = 1:numel(experiments)
                     continue;
                 end;
 
-                [reliability(j), failures{j}] = estimate_failures(trajectory, experiment_sequences{s});
+                [reliability(j), failures{j}] = estimate_failures(trajectory, event.sequence);
 
             end;            
             
-			times_file = fullfile(directory, experiment_sequences{s}.name, ...
-                sprintf('%s_time.txt', experiment_sequences{s}.name));
+			times_file = fullfile(directory, event.sequence.name, ...
+                sprintf('%s_time.txt', event.sequence.name));
 
 			if ~exist(times_file, 'file')
-				print_debug('Warning: Missing time results for tracker %s, sequence %s.', trackers{t}.identifier, experiment_sequences{s}.name);
-				continue;
+				print_debug('Warning: Missing time results for tracker %s, sequence %s.', event.tracker.identifier, event.sequence.name);
+				return;
 			end;
 
             times = csvread(times_file);
 
 			if size(times, 1) == 1
-				times = repmat(times, experiment_sequences{s}.length, 1);
+				times = repmat(times, event.sequence.length, 1);
 			end
 
 			if size(times, 2) < repeat
-				times = cat(2, times, zeros(experiment_sequences{s}.length, repeat - size(times, 2)));
+				times = cat(2, times, zeros(event.sequence.length, repeat - size(times, 2)));
 			end;
 
             valid = any(times > 0, 1) & ~isnan(reliability)';
             average_speed = mean(times(:, valid), 1)';   
             average_original = mean(average_speed);
             
-            if isfield(trackers{t}, 'performance')           
-                average_normalized = mean(normalize_speed(average_speed, failures(valid), trackers{t}, experiment_sequences{s}));
+            if isfield(event.tracker, 'performance')           
+                average_normalized = mean(normalize_speed(average_speed, failures(valid), event.tracker, event.sequence));
             else
 				average_normalized = NaN;
-                print_debug('Warning: No performance profile for tracker %s.', trackers{t}.identifier);
+                print_debug('Warning: No performance profile for tracker %s.', event.tracker.identifier);
             end;
 
             if isnan(average_normalized) || average_normalized == 0
-                normalized(e, t, s) = NaN;
+                context.normalized(event.experiment_index, event.tracker_index, event.sequence_index) = NaN;
             else
-                normalized(e, t, s) = 1 / average_normalized;
+                context.normalized(event.experiment_index, event.tracker_index, event.sequence_index) = 1 / average_normalized;
             end;
 
             if isnan(average_original) || average_original == 0
-                original(e, t, s) = NaN;
+                context.original(event.experiment_index, event.tracker_index, event.sequence_index) = NaN;
             else
-                original(e, t, s) = 1 / average_original;
-            end;            
+                context.original(event.experiment_index, event.tracker_index, event.sequence_index) = 1 / average_original;
+            end;
             
-        end;
-
     end;
 
-	print_indent(-1);
-
-end;
-
-save(cache_file, 'normalized', 'original');
+end
