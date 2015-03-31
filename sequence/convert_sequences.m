cf789f032f4275a5ccc663f2020d26a90e6bae29
Luka Čehovin <luka.cehovin@gmail.com>
2015-03-31 11:19:39 +0200

    Sequence converted is aware of already converted sequences.

--- sequence/convert_sequences.m
+++ sequence/convert_sequences.m
@@ -9,4 +9,22 @@ if ischar(converter)
     converter = str2func(converter);
 end;
 
-converted_sequences = cellfun(@(x) converter(x), sequences,'UniformOutput',false);
+converted_sequences = cellfun(@(x) convert_sequence(x, converter), sequences,'UniformOutput',false);
+
+end
+
+function sequence = convert_sequence(sequence, converter)
+
+    if isfield(sequence, 'converter')
+        % If  the sequence is already converted with this converter
+        % then do not do it again
+        if strcmp(func2str(converter), sequence.converter)
+            return;
+        end;
+    end;
+
+    sequence = converter(sequence);
+    
+    sequence.converter = func2str(converter);
+    
+end
