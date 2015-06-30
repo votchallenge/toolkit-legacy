f74709564a35bc0fd41c292bfaee5c442562f283
Luka Čehovin <luka.cehovin@gmail.com>
2015-06-30 14:26:09 +0200

    Updating Matlab integration example and utility function.
    The function now dynamically switches between legacy and new integration approach.

---
+++ tracker/examples/matlab/ncc.m
@@ -0,0 +1,124 @@
+function ncc
+% ncc VOT integration example
+% 
+% This function is an example of tracker integration into the toolkit.
+% The implemented tracker is a very simple NCC tracker that is also used as
+% the baseline tracker for challenge entries.
+%
+
+% *************************************************************
+% VOT: Always call exit command at the end to terminate Matlab!
+% *************************************************************
+%cleanup = onCleanup(@() exit() );
+
+% *************************************************************
+% VOT: Set random seed to a different value every time.
+% *************************************************************
+RandStream.setGlobalStream(RandStream('mt19937ar', 'Seed', sum(clock)));
+
+% **********************************
+% VOT: Get initialization data
+% **********************************
+[handle, image, region] = vot('rectangle');
+
+% Initialize the tracker
+[state, ~] = ncc_initialize(imread(image), region);
+
+while true
+
+    % **********************************
+    % VOT: Get next frame
+    % **********************************
+    [handle, image] = handle.frame(handle);
+
+    if isempty(image)
+        break;
+    end;
+    
+	% Perform a tracking step, obtain new region
+    [state, region] = ncc_update(state, imread(image));
+    
+    % **********************************
+    % VOT: Report position for frame
+    % **********************************
+    handle = handle.report(handle, region);
+    
+end;
+
+% **********************************
+% VOT: Output the results
+% **********************************
+handle.quit(handle);
+
+end
+
+function [state, location] = ncc_initialize(I, region, varargin)
+
+    gray = double(rgb2gray(I));
+
+    [height, width] = size(gray);
+
+    % If the provided region is a polygon ...
+    if numel(region) > 4
+        x1 = round(min(region(1:2:end)));
+        x2 = round(max(region(1:2:end)));
+        y1 = round(min(region(2:2:end)));
+        y2 = round(max(region(2:2:end)));
+        region = round([x1, y1, x2 - x1, y2 - y1]);
+    else
+        region = round([round(region(1)), round(region(2)), ... 
+            round(region(1) + region(3)) - round(region(1)), ...
+            round(region(2) + region(4)) - round(region(2))]);
+    end;
+
+    x1 = max(0, region(1));
+    y1 = max(0, region(2));
+    x2 = min(width-1, region(1) + region(3) - 1);
+    y2 = min(height-1, region(2) + region(4) - 1);
+
+    template = gray((y1:y2)+1, (x1:x2)+1);
+
+    state = struct('template', template, 'size', [x2 - x1 + 1, y2 - y1 + 1]);
+    state.window = max(state.size) * 2;
+    state.position = [x1 + x2 + 1, y1 + y2 + 1] / 2;
+
+    location = [x1, y1, state.size];
+
+end
+
+function [state, location] = ncc_update(state, I, varargin)
+
+    gray = double(rgb2gray(I)) ; 
+
+    [height, width] = size(gray);
+
+    x1 = max(0, round(state.position(1) - state.window / 2));
+    y1 = max(0, round(state.position(2) - state.window / 2));
+    x2 = min(width-1, round(state.position(1) + state.window / 2));
+    y2 = min(height-1, round(state.position(2) + state.window / 2));
+
+    region = gray((y1:y2)+1, (x1:x2)+1);
+
+    if any(size(region) < size(state.template))
+        location = [state.position - state.size / 2, state.size];
+        return;
+    end;
+
+    C = normxcorr2(state.template, region);
+
+    % We are only using valid part of the response (where full template is used)
+    pad = size(state.template) - 1;
+    center = size(region) - pad - 1;
+    C = C([false(1,pad(1)) true(1,center(1))], [false(1,pad(2)) true(1,center(2))]);
+
+    x1 = x1 + pad(2);
+    y1 = y1 + pad(1);
+    [~, imax] = max(C(:));
+    [my, mx] = ind2sub(size(C),imax(1));
+
+    position = [x1 + mx - state.size(1) / 2, y1 + my - state.size(2) / 2];
+
+    state.position = position;
+    location = [position - state.size / 2, state.size];
+
+end
---
+++ tracker/examples/matlab/vot.m
@@ -0,0 +1,234 @@
+function [handle, image, region] = vot(format)
+% vot Initialize communication and obtain communication structure
+%
+% This function is used to initialize communication with the toolkit.
+%
+% The resulting handle is a structure provides several functions for
+% further interaction:
+% - frame(handle): Get new frame from the sequence.
+% - report(handle, region): Report region for current frame and advance.
+% - quit(handle): Closes the communication and saves the data.
+%
+% Input:
+% - format (string): Desired region input format.
+%
+% Output:
+% - handle (structure): Updated communication handle structure.
+% - image (string): Path to the first image file.
+% - region (vector): Initial region encoded as a rectangle or as a polygon.
+
+    if nargin < 1
+       format = 'rectangle'; 
+    end
+
+    [handle, image, region] = tracker_initialize(format);
+    handle.frame = @tracker_frame;
+    handle.report = @tracker_report;
+    handle.quit = @tracker_quit;
+    
+end
+
+function [handle, image, region] = tracker_initialize(format)
+% tracker_initialize Initialize communication structure
+%
+% This function is used to initialize communication with the toolkit.
+%
+% Input:
+% - format (string): Desired region input format.
+%
+% Output:
+% - handle (structure): Updated communication handle structure.
+% - image (string): Path to the first image file.
+% - region (vector): Initial region encoded as a rectangle or as a polygon.
+
+    if ~ismember(format,  {'rectangle', 'polygon'})
+        error('VOT: Illegal region format.');
+    end;
+
+    if ~isempty(getenv('TRAX'))
+        % In case TraX can be used, we include the path to the server mex function.
+        if ~isempty(getenv('TRAX_MEX'))
+            addpath(getenv('TRAX_MEX'));
+        end;
+        traxserver('setup', format, 'path');
+
+        [image, region] = traxserver('wait');
+
+        handle = struct('trax', true);
+        
+        if isempty(image) || isempty(region)
+            handle.quit(handle);
+        end;
+
+        traxserver('status', region);
+        
+        return;
+    end;
+
+    handle.trax = false;
+
+    % read the image file paths
+    fid = fopen('images.txt','r'); 
+    images = textscan(fid, '%s', 'delimiter', '\n');
+    fclose(fid);
+    handle.images = images{1};
+
+    % read the region
+    region = dlmread('region.txt');
+    region = region(:);
+    handle.format = format;
+    handle.index = 2;
+    handle.regions = cell(numel(handle.images), 1);
+
+    if numel(region) == 4
+        format = 'rectangle';
+    elseif numel(region) >= 6 && mod(numel(region), 2) == 0
+        format = 'polygon';
+    else
+        error('VOT: Illegal format of the input region.');
+    end;
+
+    switch handle.format
+        case 'rectangle'
+            if strcmp(format, 'polygon')
+                x = region(1:2:end);
+                y = region(2:2:end);
+                region = [min(x), min(y), max(x) - min(x), max(y) - min(y)];
+            end;
+        case 'polygon'
+            if strcmp(format, 'rectangle')
+                x = [region(1), region(1), region(1) + region(3), ...
+                     region(1) + region(3), region(1)];
+                y = [region(2), region(2) + region(4), region(2) + ...
+                     region(4), region(2), region(2)];
+                region = zeros(8, 1);
+                region(1:2:7) = x;
+                region(2:2:8) = y;
+            end;
+    end;
+
+    handle.position = 2;
+    
+    handle.regions{1} = region;
+    image = handle.images{1};
+
+end
+
+function [handle, image] = tracker_frame(handle)
+% tracker_frame Get new frame from the sequence
+%
+% This function is used to get new frame from the current sequence
+%
+% Input:
+% - handle (structure): Communication handle structure.
+%
+% Output:
+% - handle (structure): Updated communication handle structure.
+% - image (string): Path to image file.
+
+    if ~isstruct(handle)
+        error('VOT: Handle should be a structure.');
+    end;
+
+    if handle.trax
+        [image, region] = traxserver('wait');
+
+        if isempty(image) || ~isempty(region)
+            handle.quit(handle);
+        end;
+
+        return;
+    end;
+
+    if handle.position > numel(handle.images)
+        image = [];
+        return;
+    end;
+    
+    image = handle.images{handle.position};
+
+end
+
+function handle = tracker_report(handle, region)
+% tracker_report Report region for current frame and advance
+%
+% This function stores the region for the current frame and advances
+% the internal counter to the next frame.
+%
+% Input:
+% - handle (structure): Communication handle structure.
+% - region (vector): Predicted region as a rectangle or a polygon.
+%
+% Output:
+% - handle (structure): Updated communication handle structure.
+
+    if isempty(region)
+        region = 0;
+    end;
+
+    if ~isstruct(handle)
+        error('VOT: Handle should be a structure.');
+    end;
+
+    if handle.trax
+        traxserver('status', region);
+        return;
+    end;
+
+    if handle.position > numel(handle.images)
+        return;
+    end;
+    
+    handle.regions{handle.position} = region;
+
+    handle.position = handle.position + 1;
+end
+
+
+function tracker_quit(handle)
+% tracker_quit Closes the communication and saves the data
+%
+% This function closes the communication with the toolkit and 
+% saves the remaining data.
+%
+% Input:
+% - handle (structure): Communication handle structure.
+%
+
+    if ~isstruct(handle)
+        error('VOT: Handle should be a structure.');
+    end;
+
+    if handle.trax
+        traxserver('quit');
+        return;
+    end;
+
+    if iscell(handle.regions)
+
+        fid = fopen('output.txt', 'w');
+
+        for i = 1:numel(handle.regions)
+            region = handle.regions{i};
+
+            if numel(region) == 1
+                fprintf(fid, '%f\n', region);
+            elseif numel(region) == 4
+                fprintf(fid, '%f,%f,%f,%f\n', region(1), region(2), region(3), region(4));
+            elseif numel(region) >= 6 && mod(numel(region), 2) == 0
+                fprintf(fid, '%f,', region(1:end-1));
+                fprintf(fid, '%f\n', region(end));
+            else
+                error('VOT: Illegal result format');
+            end;
+
+        end;
+
+        fclose(fid);
+
+        quit();
+    else
+        error('VOT: Unable to write results.');
+    end
+
+end
