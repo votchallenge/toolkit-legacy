64a34493d39e2959190ab849ad2cd448f8842c6f
Luka Čehovin <luka.cehovin@gmail.com>
2015-07-09 16:25:05 +0200

    Adding native directory to path if available.

--- toolkit_path.m
+++ toolkit_path.m
@@ -8,8 +8,12 @@ function paths = toolkit_path()
 % - paths (cell): A list of all paths.
 
 script_directory = fileparts(mfilename('fullpath'));
-include_dirs = cellfun(@(x) fullfile(script_directory,x), {'', 'utilities', ...
+include_dirs = cellfun(@(x) fullfile(script_directory, x), {'', 'utilities', ...
     'workspace', 'tracker', 'sequence', 'stacks' ,'report', 'analysis'}, 'UniformOutput', false); 
+
+if exist(fullfile(script_directory, 'native'), 'dir')
+   include_dirs{end+1} = fullfile(script_directory, 'native');
+end
 
 if nargout > 0
     paths = include_dirs;
