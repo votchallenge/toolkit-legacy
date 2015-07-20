9d2c75c7a6f10e9533a35d33bc3f763e6bbf1ccd
Georg Nebehay <gnebehay@gmail.com>
2015-07-20 10:47:18 +0200

    Fixed usage of __has_include

--- tracker/examples/native/vot.h
+++ tracker/examples/native/vot.h
@@ -44,7 +44,7 @@
 
 // Newer compilers support interactive checks for headers, otherwise we have to enable TraX support manually
 #ifdef __has_include
-#  if __has_include(trax.h)
+#  if __has_include("trax.h")
 #    include <trax.h>
 #    define VOT_TRAX
 #  endif
