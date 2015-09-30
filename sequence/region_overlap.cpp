526cbb708b9276560d204212c4d5df4fac7e9f58
Luka Čehovin <luka.cehovin@gmail.com>
2015-09-30 17:03:57 +0200

    Calculating overlaps for trajectories in MEX directly. Up to 3x speedup when doing a lot of calculations.

--- sequence/region_overlap.cpp
+++ sequence/region_overlap.cpp
@@ -8,10 +8,10 @@ region_container* get_polygon(const mxArray * input) {
     
     region_container* p = NULL;
 	double *r = (double*)mxGetPr(input);
-    int l = mxGetN(input);
-    
+    int l = MAX(mxGetN(input), mxGetM(input));
+
     if (l % 2 == 0 && l > 6) {
-        
+
         p = region_create_polygon(l / 2);
         
         for (int i = 0; i < p->data.polygon.count; i++) {
@@ -39,48 +39,94 @@ region_container* get_polygon(const mxArray * input) {
     
 }
 
-
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+region_overlap compute_overlap(const mxArray* r1, const mxArray* r2) {
 
 	region_container* p1;
 	region_container* p2;
 
-	if( nrhs != 2 ) mexErrMsgTxt("Two vector arguments (regions) required.");
-	if( nlhs != 1 ) mexErrMsgTxt("Exactly one output argument required.");
+    if ( mxGetNumberOfDimensions(r1) > 2 || mxGetM(r1) > 1 ) mexErrMsgTxt("All regions must be vectors");
+    if ( mxGetNumberOfDimensions(r2) > 2 || mxGetM(r2) > 1 ) mexErrMsgTxt("All regions must be vectors");
 
-	for (int i = 0; i < 2; i++) {
-        // TODO: accept integer for special frames
-		if (mxGetClassID(prhs[i]) != mxDOUBLE_CLASS)
-			mexErrMsgTxt("All input arguments must be of type double");
-	
-		if ( mxGetNumberOfDimensions(prhs[i]) > 2 || mxGetM(prhs[i]) > 1 ) mexErrMsgTxt("All input arguments must be vectors");
+    // TODO: accept integer for special frames
+    if (mxGetClassID(r1) != mxDOUBLE_CLASS)
+	    mexErrMsgTxt("All input arguments must be of type double");
 
-	}
+    if (mxGetClassID(r2) != mxDOUBLE_CLASS)
+	    mexErrMsgTxt("All input arguments must be of type double");
 
-	p1 = get_polygon(prhs[0]);
-    p2 = get_polygon(prhs[1]);
+    p1 = get_polygon(r1);
+    p2 = get_polygon(r2);
     
-    plhs[0] = mxCreateDoubleMatrix(1, 3, mxREAL);
-    double *result = (double*) mxGetPr(plhs[0]);
-            
-    if (p1 && p2) {
-        
-        region_overlap overlap = region_compute_overlap(p1, p2);
+    region_overlap overlap;
 
-        result[0] = overlap.overlap;
-        result[1] = overlap.only1;
-        result[2] = overlap.only2;
-        
+    if (p1 != NULL && p2 != NULL) {
+
+        overlap = region_compute_overlap(p1, p2);
+	   
     } else {
-        
-        result[0] = mxGetNaN();
-        result[1] = mxGetNaN();
-        result[2] = mxGetNaN();
-        
+
+        overlap.overlap = -1;
+      
     }
     
     if (p1) region_release(&p1);
     if (p2) region_release(&p2);
+
+    return overlap;
+}
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+
+	if( nrhs != 2 ) mexErrMsgTxt("Two vector or cell arguments (regions) required.");
+	if( nlhs != 1 ) mexErrMsgTxt("Exactly one output argument required.");
+
+    if (mxIsCell(prhs[0]) && mxIsCell(prhs[1])) {
+	
+        if ( MIN(mxGetM(prhs[0]), mxGetN(prhs[0])) != 1 ) mexErrMsgTxt("Cell array must be a vector");
+        if ( MIN(mxGetM(prhs[1]), mxGetN(prhs[1])) != 1 ) mexErrMsgTxt("Cell array must be a vector");
+
+        if ( MAX(mxGetM(prhs[0]), mxGetN(prhs[0])) != MAX(mxGetM(prhs[1]), mxGetN(prhs[1])))
+            mexErrMsgTxt("Cell arrays must be of equal size");
+
+        int num = MAX(mxGetM(prhs[0]), mxGetN(prhs[0]));
+
+        plhs[0] = mxCreateDoubleMatrix(num, 3, mxREAL);
+        double *result = (double*) mxGetPr(plhs[0]);
+
+        for (int i = 0; i < num; i++) {
+            mxArray* r1 = mxGetCell (prhs[0], i);
+            mxArray* r2 = mxGetCell (prhs[1], i);
+
+            region_overlap overlap = compute_overlap(r1, r2);
+
+            if (overlap.overlap < 0) {
+                result[i] = mxGetNaN();
+                result[i + num] = mxGetNaN();
+                result[i + num * 2] = mxGetNaN();
+            } else {
+                result[i] = overlap.overlap;
+                result[i + num] = overlap.only1;
+                result[i + num * 2] = overlap.only2;
+            }
+        }
+
+
+    } else {
+
+        plhs[0] = mxCreateDoubleMatrix(1, 3, mxREAL);
+        double *result = (double*) mxGetPr(plhs[0]);
+                
+        region_overlap overlap = compute_overlap(prhs[0], prhs[1]);
+        if (overlap.overlap < 0) {
+            result[0] = mxGetNaN();
+            result[1] = mxGetNaN();
+            result[2] = mxGetNaN();
+        } else {
+            result[0] = overlap.overlap;
+            result[1] = overlap.only1;
+            result[2] = overlap.only2;
+        }
+    }
 
 }
 
