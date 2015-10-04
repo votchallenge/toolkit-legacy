aa4f5b550c089d5beed9829302e7e637c75ed428
Luka Čehovin <luka.cehovin@gmail.com>
2015-10-04 11:52:58 +0200

    Weighting sequences based on the number of results.

--- analysis/estimate_expected_overlap.m
+++ analysis/estimate_expected_overlap.m
@@ -31,10 +31,13 @@ end
 context.failures = {};
 context.overlaps = {};
 context.practical = {};
+context.sources = [];
 context = iterate(experiment, tracker, sequences, 'iterator', @collect_segments, 'context', context);
 failures = context.failures;
 overlaps = context.overlaps;
 practical = context.practical;
+occurences = hist(context.sources, max(context.sources));
+weights = 1 ./ occurences(context.sources);
 
 if isempty(lengths)
     maxlen = max(cellfun(@(x) numel(x), overlaps, 'UniformOutput', true));
@@ -51,6 +54,7 @@ fragments_length = max(lengths);
 
 fragments = nan(fragments_count, fragments_length);
 fpractical = nan(fragments_count, fragments_length);
+fweights = nan(fragments_count, 1);
 f = 1;
 for i = 1:numel(overlaps)
     % calculate number of failures and their positions in the trajectory
@@ -69,6 +73,8 @@ for i = 1:numel(overlaps)
             fpractical(f, :) = 0;
             fpractical(f, 1:min(numel(o), fragments_length)) = o;
             
+            fweights(f) = weights(i);
+            
             f = f + 1;
         end;
 
@@ -76,6 +82,9 @@ for i = 1:numel(overlaps)
         fragments(f, 1:min(numel(o), fragments_length)) = o;
         o = practical{i}(points(end):end); o(isnan(o)) = 0;
         fpractical(f, 1:min(numel(o), fragments_length)) = o;
+        
+        fweights(f) = weights(i);
+        
         f = f + 1;
     else
     % process also last part of the trajectory - segment without failure
@@ -88,13 +97,13 @@ for i = 1:numel(overlaps)
             fragments(f, 1:numel(overlaps{i})) = overlaps{i};
             fpractical(f, 1:numel(practical{i})) = practical{i};
         end
+        fweights(f) = weights(i);
         f = f + 1;
     end
 end
 
 % w is vector of weights (can be used in per-visual property calculation)
 % if weights are not given - use equal weights
-w = ones(size(fragments, 1), 1);
 
 expected_overlaps = zeros(numel(lengths), 1);
 practical_difference = zeros(numel(lengths), 1);
@@ -109,8 +118,8 @@ for e = 1:numel(expected_overlaps)
     usable = ~isnan(fragments(:, len));
     
     % for each len get a single number - average overlap
-    expected_overlaps(e) = sum(mean(fragments(usable, 2:len), 2) .* w(usable)) ./ sum(w(usable));
-    practical_difference(e) = sum(mean(fpractical(usable, 2:len), 2) .* w(usable)) ./ sum(w(usable));
+    expected_overlaps(e) = sum(mean(fragments(usable, 2:len), 2) .* fweights(usable)) ./ sum(fweights(usable));
+    practical_difference(e) = sum(mean(fpractical(usable, 2:len), 2) .* fweights(usable)) ./ sum(fweights(usable));
 end
 
 evaluated_lengths = lengths;
@@ -134,13 +143,13 @@ switch (event.type)
                 for i = 1:event.experiment.parameters.repetitions
                     
                     result_file = fullfile(sequence_directory, sprintf('%s_%03d.txt', event.sequence.name, i));
+
+                    if i == 4 && is_deterministic(sequence, 3, sequence_directory)
+                        break;
+                    end;
                     
                     if ~exist(result_file, 'file')
                         continue;
-                    end;
-                    
-                    if i == 4 && is_deterministic(sequence, 3, sequence_directory)
-                        break;
                     end;
                     
                     trajectory = read_trajectory(result_file);
@@ -151,8 +160,9 @@ switch (event.type)
 
                     practical = get_frame_value(sequence, 'practical');
                     
-                    context.failures{end+1} = failures;
+                    context.failures{end+1} = failures(failures <= sequence.length);
                     context.overlaps{end+1} = frames;
+                    context.sources(end+1) = event.sequence_index;
                     
                     if isempty(practical)
                         context.practical{end+1} = zeros(sequence.length, 1);
