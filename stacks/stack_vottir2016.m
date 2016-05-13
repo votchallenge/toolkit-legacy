e7f974f74150fc2fbea40fc279f58fc104ea8397
Gustav Häger <gustav.hager@liu.se>
2016-05-13 13:04:39 +0200

    Add VOTTIR 2016 stack

---
+++ stacks/stack_vottir2016.m
@@ -0,0 +1,17 @@
+function experiments = stack_vottir2015()
+
+set_global_variable('bundle', 'http://www.cvl.isy.liu.se/research/datasets/ltir/version1.0/ltir_v1_0_8bit.zip');
+
+baseline.name = 'baseline';
+baseline.converter = [];
+baseline.type = 'supervised';
+baseline.labels = {'camera_motion', 'dynamics_change', 'occlusion', 'size','motion', 'empty'};
+               
+baseline.parameters.repetitions = 15;
+baseline.parameters.burnin = 10;
+baseline.parameters.skip_initialize = 5;
+baseline.parameters.failure_overlap = 0;
+
+experiments = {baseline};
+
+end
