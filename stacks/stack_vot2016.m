cd83428263a79a09e1fa784241d47fcad03f3e4e
Alan Lukežič <alan.lukezic@gmail.com>
2016-05-24 14:34:58 +0200

    Added vot2016 stack file

---
+++ stacks/stack_vot2016.m
@@ -0,0 +1,28 @@
+function experiments = stack_vot2016()
+
+set_global_variable('bundle', 'http://box.vicos.si/vot/vot2016.zip');
+
+baseline.name = 'baseline';
+baseline.converter = [];
+baseline.type = 'supervised';
+baseline.labels = {'camera_motion', 'illum_change', 'occlusion', 'size_change', 'motion_change', 'empty'};
+
+baseline.parameters.repetitions = 15;
+baseline.parameters.burnin = 10;
+baseline.parameters.skip_initialize = 5;
+baseline.parameters.failure_overlap = 0;
+
+unsupervised.name = 'unsupervised';
+unsupervised.converter = [];
+unsupervised.type = 'supervised';
+unsupervised.labels = {'camera_motion', 'illum_change', 'occlusion', 'size_change', 'motion_change', 'empty'};
+
+unsupervised.parameters.repetitions = 1;
+unsupervised.parameters.burnin = 0;
+unsupervised.parameters.skip_initialize = 0;
+unsupervised.parameters.failure_overlap = -1;
+
+experiments = {baseline, unsupervised};
+
+end
+
