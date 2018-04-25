981a45bc402be285006eeb6317f29448149b3ae5
Luka Čehovin <luka.cehovin@gmail.com>
2018-04-25 15:41:00 +0200

    Fixing caching in redetection experiment.

--- sequence/sequence_test_redetection.m
+++ sequence/sequence_test_redetection.m
@@ -2,14 +2,14 @@ function out_sequence = sequence_test_redetection(sequence, varargin)
 
 new_length = 200;
 init_frames = 5;
-pad = 2;
+padding = 2;
 enlarge_image_factor = 3;
 
 for j=1:2:length(varargin)
     switch lower(varargin{j})
         case 'length', new_length = varargin{j+1};
         case 'initialization', init_frames = varargin{j+1};
-        case 'padding', pad = varargin{j+1};
+        case 'padding', padding = varargin{j+1};
         case 'scaling', enlarge_image_factor = varargin{j+1};
         otherwise, error(['unrecognized argument ' varargin{j}]);
     end
@@ -22,63 +22,59 @@ mkpath(cache_directory);
 
 cache_groundtruth = fullfile(cache_directory, 'groundtruth.txt');
 
-print_debug('Generating cached long-term check sequence ''%s''...', sequence.name);
+if ~exist(cache_groundtruth, 'file')
 
-image = imread(get_image(sequence, 1));
-gt_region = sequence.groundtruth{1};
-target_template = get_patch(image, gt_region, pad);
+    print_debug('Generating cached long-term check sequence ''%s''...', sequence.name);
 
-redet_img = uint8(zeros(enlarge_image_factor * size(image,1), ...
-    enlarge_image_factor * size(image,2), size(image,3)));
+    image = imread(get_image(sequence, 1));
+    initial_region = sequence.groundtruth{1};
+    target_template = get_patch(image, initial_region, padding);
 
-x0 = size(redet_img,2) - size(target_template,2) + 1;
-y0 = size(redet_img,1) - size(target_template,1) + 1;
-x1 = size(redet_img,2);
-y1 = size(redet_img,1);
+    redetection_image = uint8(zeros(enlarge_image_factor * size(image,1), ...
+        enlarge_image_factor * size(image,2), size(image,3)));
 
-redet_img(y0:y1, x0:x1, :) = target_template;
+    x0 = size(redetection_image,2) - size(target_template,2) + 1;
+    y0 = size(redetection_image,1) - size(target_template,1) + 1;
+    x1 = size(redetection_image,2);
+    y1 = size(redetection_image,1);
 
-cx = (x0 + x1) / 2;
-cy = (y0 + y1) / 2;
-gt_x0 = round(cx - gt_region(3) / 2);
-gt_y0 = round(cy - gt_region(4) / 2);
-gt_redet = [gt_x0, gt_y0, gt_region(3), gt_region(4)];
+    redetection_image(y0:y1, x0:x1, :) = target_template;
 
-img0 = uint8(zeros(size(redet_img)));
-img0(1:size(image,1), 1:size(image,2), :) = image;
+    cx = (x0 + x1) / 2;
+    cy = (y0 + y1) / 2;
+    gt_x0 = round(cx - initial_region(3) / 2);
+    gt_y0 = round(cy - initial_region(4) / 2);
+    redetection_region = [gt_x0, gt_y0, initial_region(3), initial_region(4)];
 
-imwrite(img0, fullfile(cache_directory, 'init_image.jpg'));
-imwrite(redet_img, fullfile(cache_directory, 'redet_image.jpg'));
+    initial_image = uint8(zeros(size(redetection_image)));
+    initial_image(1:size(image,1), 1:size(image,2), :) = image;
 
-sequence_length = min(new_length, sequence.length);
-gt_new = cell(sequence_length, 1);
-images_new = cell(sequence_length, 1);
-for i = 1:sequence_length
+    imwrite(initial_image, fullfile(cache_directory, 'initial.jpg'));
+    imwrite(redetection_image, fullfile(cache_directory, 'redetection.jpg'));
 
-    if i <= init_frames
-        gt_new{i} = gt_region;
-        images_new{i} = 'init_image.jpg';
-    else
-        gt_new{i} = gt_redet;
-        images_new{i} = 'redet_image.jpg';
-    end
-
+    write_trajectory(cache_groundtruth, {initial_region, redetection_region});
+    
 end;
 
-out_sequence = sequence;
+sequence_length = min(new_length, sequence.length);
 
-out_sequence.images = images_new;
-out_sequence.groundtruth = gt_new;
+image = imread(fullfile(cache_directory, 'initial.jpg'));
+
+groundtruth = read_trajectory(cache_groundtruth);
+
+groundtruth = cat(1, repmat(groundtruth(1), init_frames, 1), ...
+    repmat(groundtruth(2), sequence_length - init_frames, 1));
+images = cat(1, repmat({'initial.jpg'}, init_frames, 1), ...
+    repmat({'redetection.jpg'}, sequence_length - init_frames, 1));
+
+out_sequence = sequence;
+out_sequence.images = images;
+out_sequence.groundtruth = groundtruth;
 out_sequence.length = sequence_length;
-out_sequence.width = size(redet_img, 2);
-out_sequence.height = size(redet_img, 1);
+out_sequence.width = size(image, 2);
+out_sequence.height = size(image, 1);
 out_sequence.indices = sequence.indices(1:sequence_length);
 out_sequence.images_directory = cache_directory;
-
-write_trajectory(cache_groundtruth, out_sequence.groundtruth);
-
-out_sequence.tags.names = sequence.tags.names;
-out_sequence.tags.data = sequence.tags.data;
 
 end
 
